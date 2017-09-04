7c623e1efebab848fa2331249e7c746630fcd07c
Nikola Jichev <njichev@gmail.com>
2017-09-04 20:50:17 +0300

    Add more lambda.hs

--- lambda.hs
+++ lambda.hs
@@ -1,28 +1,14 @@
--- Lambda Terms
--- data Var = X | Y
--- data LambdaTerm = Var
---                 | Application LambdaTerm LambdaTerm
---                 | Abstraction Var LambdaTerm
---
--- t1 :: LambdaTerm
--- t1 = X
---
--- t2 :: LambdaTerm
--- t2 = Var Y
---
--- term3 :: LambdaTerm
--- term3 = Application t1 t2
---
 import Data.List
 
 type Name = String
 
+-- Our Lambda Terms(Named ones)
 data Term = Var Name
           | App Term Term
           | Abs Name Term
 
 instance Show Term where
-  show (Var a) = "'" ++ a ++ "'"
+  show (Var a) = a
   show (App t1 t2) = "(" ++ show t1 ++ show t2 ++ ")"
   show (Abs var term) = "λ" ++ var ++ "(" ++ show term ++ ")"
 
@@ -62,5 +48,36 @@ sub (Abs y t1) x t2
   | otherwise = error "Undefined"
 
 
-rename :: Term -> Name -> Term
+-- Unnamed lambda terms
+data UTerm = UVar Int
+           | UApp UTerm UTerm
+           | UAbs UTerm
+           deriving (Eq, Read)
 
+instance Show UTerm where
+  show (UVar i) = show i
+  show (UApp t1 t2) = show t1 ++ show t2
+  show (UAbs term) = "(" ++ "λ" ++ " " ++ show term ++ ")"
+
+generator :: [Name]
+generator = [ [i] | i <- letters] ++ [i: show j | j <- [1..], i <- letters]
+  where letters = ['x', 'y', 'z', 't', 'u', 'v']
+
+generate :: [Name] -> Name
+generate used = head (generator \\ used)
+
+-- Проверки
+-- • термът λxy се представя като λ1
+-- • термът y(λxxyz)z се представя като 0(λ013)2,
+-- • термът (λxxy(λuzu(λvvy))z) се представя като (λ01(λ40(λ03))3).
+-- • термът λ(λ0((λ1)21))1 отговаря на λx(λtt((λut)yx))y.
+--
+-- name :: UTerm -> Term
+-- name = name'
+--   where
+--     name' :: 
+--
+-- unname :: Term -> UTerm
+
+-- We want
+-- unname(\x -> x) == \ 0
