91bbcced9dfaf1ccdbde5aab72853aef3c3a6654
Nikola Jichev <njichev@gmail.com>
2017-09-04 12:42:21 +0300

    Implement hilbert system

--- hilbert.hs
+++ hilbert.hs
@@ -1,21 +1,112 @@
+data EFormula = EVar String
+              | ENot EFormula
+              | EImpl EFormula EFormula
+              | EAnd EFormula EFormula
+              | EOr EFormula EFormula
+              | EEvery String EFormula
+              | EExist String EFormula
+              deriving (Eq)
 
 data Formula = Var String
              | Not Formula
-             | Impl Formula Formula deriving (Eq)
+             | Impl Formula Formula
+             | Every String Formula
+             deriving (Eq)
 
-type Interp = String -> Maybe Bool
+intern :: EFormula -> Formula
+intern (EVar str) = (Var str)
+intern (ENot fi) = (Not (intern fi))
+intern (EImpl fi psi) = (Impl (intern fi) (intern psi))
+intern (EEvery str fi) = (Every str (intern fi))
+intern (EOr fi psi) = (Impl (Not (intern fi)) (intern psi))
+intern (EAnd fi psi) = (Not (Impl (intern fi) (Not (intern psi))))
+intern (EExist str fi) = (Not (Every str (Not (intern fi))))
 
-eval :: Formula -> Interp -> Maybe Bool
-eval (Var fi) i = i fi
-eval (Not fi) i = (eval fi i) >>= notmonadic
-eval _ _ = Nothing
+instance Show Formula where
+  show (Var var) = var
+  show (Not fi) = "(" ++ "¬" ++ show fi ++ ")"
+  show (Impl fi psi) = "(" ++ show fi ++ "→" ++ show psi ++ ")"
+  show (Every var fi) = "(" ++ "∀" ++ var ++ show(fi) ++ ")"
 
-notmonadic :: Bool -> Maybe Bool
-notmonadic True = Just False
-notmonadic False= Just True
+instance Show EFormula where
+  show (EVar var) = var
+  show (ENot fi) = "(" ++ "¬" ++ show fi ++ ")"
+  show (EImpl fi psi) = "(" ++ show fi ++ "→" ++ show psi ++ ")"
+  show (EAnd fi psi) = "(" ++ show fi ++ "∧" ++ show psi ++ ")"
+  show (EOr fi psi) = "(" ++ show fi ++ "∨" ++ show psi ++ ")"
+  show (EExist var fi) = "(" ++ "∃" ++ var ++ show(fi) ++ ")"
+  show (EEvery var fi) = "(" ++ "∀" ++ var ++ show(fi) ++ ")"
 
-ip :: Interp
-ip "p1" = Just True
-ip "p2" = Just True
-ip "p3" = Just False
-ip _ = Nothing
+eq :: EFormula -> Formula -> Bool
+eq efor for = (intern efor) == for
+
+isAxiom :: Formula -> Bool
+isAxiom f = or (map ($ f) axioms)
+-- isAxim fi = any (==True) . 
+
+axioms :: [Formula -> Bool]
+axioms = [isAxiom1, isAxiom2]
+
+isAxiom1 :: Formula -> Bool
+isAxiom1 (Impl (Impl fi1 (Impl psi1 xi1)) (Impl (Impl fi2 psi2) (Impl fi3 xi2))) = fi1 == fi2 && fi2 == fi3 &&
+                          psi1 == psi2 &&
+                          xi1 == xi2
+isAxiom1 _ = False
+
+isAxiom2 :: Formula -> Bool
+isAxiom2 (Impl fi (Impl psi xi)) = fi == xi
+isAxiom2 _ = False
+
+isAxiom3 :: Formula -> Bool
+isAxiom3 (Impl (Every x fi) psi) = 
+isAxiom3 _ = False
+
+(~=) :: Formula -> Formula -> Bool
+(~=) (Var x) (Var y) = True
+(~=) (Not fi) (Not psi) = fi ~= psi
+(~=) (Impl fi1 psi1) (Impl fi2 psi2) = fi1 ~= fi2 &&
+                                       psi1 ~= psi2
+(~=) (Not fi) (Not psi) = fi ~= psi
+(~=) (Every x fi) (Every y psi) = x == y &&
+                                  fi ~= psi
+(~=) _ _ = False
+
+type Formulae = [Formula]
+type Hypotheses = Formulae
+
+isDeduction :: Hypotheses -> Formulae -> Bool
+isDeduction hyp fs = isDeduction' hyp [] fs
+
+-- isDeduction :: [EFormula] -> [EFormula] -> Bool
+-- isDeduction hyp fs = isDeduction' hyp' [] fs'
+--   where
+--     hyp' = map intern hyp
+--     fs'  = map intern fs
+--
+isDeduction' :: Hypotheses -> Formulae -> Formulae -> Bool
+isDeduction' hyp proved [] = True
+isDeduction' hyp proved (f:fs)
+  | isAxiom f              = proveNext
+  | elem f hyp             = proveNext
+  | isModusPonens proved f = proveNext
+  | otherwise              = False
+  where proveNext = isDeduction' hyp (f : proved) fs
+
+-- isTautology :: [EFormula] -> Bool
+-- isTautology = isDeduction []
+--
+isModusPonens :: Formulae -> Formula -> Bool
+isModusPonens proved f = any (\ (Impl x _) -> elem x proved) impls
+  where impls = [impl | impl@(Impl _ fi) <- proved,
+                        fi == f]
+
+x = Var "x"
+target = (Impl x x)
+
+proof = [
+  (Impl x (Impl target x)),
+  (Impl (Impl x (Impl target x)) (Impl (Impl x target) (Impl x x))),
+  (Impl (Impl x target) (Impl x x)),
+  (Impl x (Impl x x)),
+  target
+  ]
--- lambda.hs
+++ lambda.hs
@@ -4,18 +4,63 @@
 --                 | Application LambdaTerm LambdaTerm
 --                 | Abstraction Var LambdaTerm
 --
--- term1 :: LambdaTerm
--- term1 = X
+-- t1 :: LambdaTerm
+-- t1 = X
 --
--- term2 :: LambdaTerm
--- term2 = Var Y
+-- t2 :: LambdaTerm
+-- t2 = Var Y
 --
 -- term3 :: LambdaTerm
--- term3 = Application term1 term2
+-- term3 = Application t1 t2
 --
-data Name = Var String
+import Data.List
 
-data Term = Name
+type Name = String
+
+data Term = Var Name
           | App Term Term
           | Abs Name Term
-          deriving(Eq, Show)
+
+instance Show Term where
+  show (Var a) = "'" ++ a ++ "'"
+  show (App t1 t2) = "(" ++ show t1 ++ show t2 ++ ")"
+  show (Abs var term) = "λ" ++ var ++ "(" ++ show term ++ ")"
+
+
+-- Examples
+-- x :: Term
+-- x = Var "x"
+identity :: Term
+identity = Abs "x" (Var "x")
+
+constant :: Term
+constant = Abs "x" (Var "y")
+
+-- end examples
+
+freeVars :: Term -> [Name]
+freeVars (Var var) = [var]
+freeVars (App t1 t2) = freeVars(t1) ++ freeVars(t2)
+freeVars (Abs var term) = freeVars(term) \\ [var]
+
+boundVars :: Term -> [Name]
+boundVars (Var var) = []
+boundVars (App t1 t2) = boundVars(t1) ++ boundVars(t2)
+boundVars (Abs var term) = boundVars(term) ++ [var]
+
+vars :: Term -> [Name]
+vars term = freeVars(term) ++ boundVars(term)
+
+sub :: Term -> Name -> Term -> Term
+sub (Var x) y t2
+  | x == y    = t2
+  | otherwise = (Var x)
+sub (App t1 t2) y t3 = (App (sub t1 y t3) (sub t2 y t3))
+sub (Abs y t1) x t2
+  | x == y = (Abs y t1)
+  | not(elem x (vars t1)) && not(elem y (freeVars t2)) = (Abs y t1)
+  | otherwise = error "Undefined"
+
+
+rename :: Term -> Name -> Term
+
