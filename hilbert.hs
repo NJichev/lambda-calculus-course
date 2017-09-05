da7a4100cf5c71a0e91ac99f0631a5e48c9a5c8d
Nikola Jichev <njichev@gmail.com>
2017-09-05 17:04:47 +0300

    Implement rest of the axioms in our internal repr.
    This will help us prove harder hypothesis using more functions.

--- hilbert.hs
+++ hilbert.hs
@@ -1,17 +1,39 @@
-data EFormula = EVar String
+import Data.List
+
+type Name = String
+type Names = [Name]
+
+data EFormula = EVar Name
               | ENot EFormula
               | EImpl EFormula EFormula
               | EAnd EFormula EFormula
               | EOr EFormula EFormula
-              | EEvery String EFormula
-              | EExist String EFormula
+              | EEvery Name EFormula
+              | EExist Name EFormula
               deriving (Eq)
 
-data Formula = Var String
+data Formula = Var Name
              | Not Formula
              | Impl Formula Formula
-             | Every String Formula
+             | Every Name Formula
              deriving (Eq)
+
+-- Vars
+freeVars :: Formula -> Names
+freeVars (Var var)     = [var]
+freeVars (Not fi)      = freeVars fi
+freeVars (Impl fi psi) = (freeVars fi) ++ (freeVars psi)
+freeVars (Every x fi)  = (freeVars fi) \\ [x]
+
+boundVars :: Formula -> Names
+boundVars (Var _)       = []
+boundVars (Not fi)      = boundVars fi
+boundVars (Impl fi psi) = (boundVars fi) ++ (boundVars psi)
+boundVars (Every x fi)  = (boundVars fi) ++ [x]
+
+vars :: Formula -> Names
+vars fi = freeVars(fi) ++ boundVars(fi)
+
 
 intern :: EFormula -> Formula
 intern (EVar str) = (Var str)
@@ -42,31 +64,90 @@ eq efor for = (intern efor) == for
 
 isAxiom :: Formula -> Bool
 isAxiom f = or (map ($ f) axioms)
--- isAxim fi = any (==True) . 
 
 axioms :: [Formula -> Bool]
-axioms = [isAxiom1, isAxiom2]
+axioms = [isImplAxiom1, isImplAxiom2]
 
-isAxiom1 :: Formula -> Bool
-isAxiom1 (Impl (Impl fi1 (Impl psi1 xi1)) (Impl (Impl fi2 psi2) (Impl fi3 xi2))) = fi1 == fi2 && fi2 == fi3 &&
-                          psi1 == psi2 &&
-                          xi1 == xi2
-isAxiom1 _ = False
+-- Axioms for ->
+-- 1. (A → B → C) → (A → B) → A → C
+isImplAxiom1 :: Formula -> Bool
+isImplAxiom1 (Impl (Impl fi1 (Impl psi1 xi1)) (Impl (Impl fi2 psi2) (Impl fi3 xi2))) = fi1 == fi2 && fi2 == fi3 &&
+                               psi1 == psi2 &&
+                               xi1 == xi2
+isImplAxiom1 _ = False
 
-isAxiom2 :: Formula -> Bool
-isAxiom2 (Impl fi (Impl psi xi)) = fi == xi
-isAxiom2 _ = False
+-- 2. A → B → A
+isImplAxiom2 :: Formula -> Bool
+isImplAxiom2 (Impl fi (Impl psi xi)) = fi == xi
+isImplAxiom2 _ = False
 
-isAxiom3 :: Formula -> Bool
-isAxiom3 (Impl (Every x fi) psi) = 
-isAxiom3 _ = False
+-- Axioms for && converted to -> and !
+-- 3 A ∧ B → A, A ∧ B → B
+isAndAxiom1 :: Formula -> Bool
+isAndAxiom1 (Impl (Not (Impl fi (Not psi))) xi) = fi == xi || psi == xi
+isAndAxiom1 _ = False
 
+-- 4 A → B → A ∧ B
+isAndAxiom2 :: Formula -> Bool
+isAndAxiom2 (Impl (Impl fi1 psi1) (Not (Impl fi2 (Not psi2)))) = fi1 == fi2 && psi1 == psi2
+isAndAxiom2 _ = False
+
+--
+--5 A → A ∨ B, B → A ∨ B
+isOrAxiom1 :: Formula -> Bool
+isOrAxiom1 (Impl fi (Impl (Not psi) xi)) = fi == psi || fi == xi
+isOrAxiom1 _ = False
+
+--6 ((A → C) → (B → C)) → ((A ∨ B) → C)
+isOrAxiom2 :: Formula -> Bool
+isOrAxiom2 (Impl (Impl (Impl a1 c1) (Impl b1 c2)) (Impl (Impl (Not a2) b2) c3)) = a1 == a2 && b1 == b2 && c1 == c2 && c2 == c3
+isOrAxiom2 _ = False
+
+
+-- 7 ∀xA → A[x 7→ t]
+isEveryAxiom1 :: Formula -> Bool
+isEveryAxiom1 (Impl (Every str fi) psi) = any (\ new -> (simpleRename fi str new) == psi) . vars $ psi
+isEveryAxiom1 _ = False
+
+-- Rename everything to check for equality in 7.
+simpleRename :: Formula -> Name -> Name -> Formula
+simpleRename (Var name)    old new
+  | old == name = Var new
+  | otherwise   = Var name
+simpleRename (Not fi)      old new = Not (simpleRename fi old new)
+simpleRename (Impl fi psi) old new = Impl (simpleRename fi old new) (simpleRename psi old new)
+simpleRename (Every x fi)  old new
+  | old == x  = (Every new (simpleRename fi old new))
+  | otherwise = (Every x (simpleRename fi old new))
+
+-- 8 ∀x (B → A) → (B → ∀xA), ако x ∈/ FV(B)
+isEveryAxiom2 :: Formula -> Bool
+isEveryAxiom2 (Impl (Every x (Impl b1 a1)) (Impl b2 (Every y a2))) = x == y && b1 == b2 && a1 == a2 && (not $ elem x (freeVars b2))
+isEveryAxiom2 _ = False
+
+-- 9 A[x 7→ t] → ∃xA
+isExistAxiom1 :: Formula -> Bool
+isExistAxiom1 (Impl b (Not (Every x (Not a)))) = any (\ t -> (simpleRename a x t) == b) . vars $ b
+isExistAxiom1 _ = False
+
+-- 10 ∀x (A → B) → (∃xA → B), ако x ∈/ FV(B)
+isExistAxiom2 :: Formula -> Bool
+isExistAxiom2 (Impl (Every x (Impl a1 b1)) (Impl (Not (Every y (Not a2))) b2)) = x == y && a1 == a2 && b1 == b2 && (not $ elem x (vars b1))
+isExistaxiom2 _ -> False
+
+-- Test vars for the axioms
+a = EVar "a"
+b = EVar "b"
+
+test1 = intern $ (EImpl (EAnd a b) a)
+test2 = intern $ (EImpl (EAnd a b) b)
+
+-- This tells us whether two formulas have the same structure
 (~=) :: Formula -> Formula -> Bool
 (~=) (Var x) (Var y) = True
 (~=) (Not fi) (Not psi) = fi ~= psi
 (~=) (Impl fi1 psi1) (Impl fi2 psi2) = fi1 ~= fi2 &&
                                        psi1 ~= psi2
-(~=) (Not fi) (Not psi) = fi ~= psi
 (~=) (Every x fi) (Every y psi) = x == y &&
                                   fi ~= psi
 (~=) _ _ = False
@@ -77,12 +158,6 @@ type Hypotheses = Formulae
 isDeduction :: Hypotheses -> Formulae -> Bool
 isDeduction hyp fs = isDeduction' hyp [] fs
 
--- isDeduction :: [EFormula] -> [EFormula] -> Bool
--- isDeduction hyp fs = isDeduction' hyp' [] fs'
---   where
---     hyp' = map intern hyp
---     fs'  = map intern fs
---
 isDeduction' :: Hypotheses -> Formulae -> Formulae -> Bool
 isDeduction' hyp proved [] = True
 isDeduction' hyp proved (f:fs)
